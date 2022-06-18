2aba1f1229f8988de1741c6baf2050c325118b5c
Volodimir <terdev94@gmail.com>
2022-06-18 16:11:22 +0300

    Исправил свои ошибки. Удалось добавить PhpMyAdmin в контейнер

--- www/database/data_base.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$conn = mysqli_connect('loclhost', 'root', 'root');
-if (!$conn->connect_error) {
-    die("Ошибка соединения: " . $conn->connect_error);
-}
-echo "Успешно соединились";
-$conn->close();
-?>
