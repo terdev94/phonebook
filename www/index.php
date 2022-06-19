2b63d360d3354ef8be8f95b3e691c113660ba015
Volodimir <terdev94@gmail.com>
2022-06-19 14:50:59 +0300

    Создал таблицу в БД из 4-рех столбцов

--- www/index.php
+++ www/index.php
@@ -3,11 +3,16 @@
   <meta charset="UTF-8">
 
 <?php
-$conn = mysqli_connect("database", "phonebook", "phonebook");
+$conn = mysqli_connect("database", "phonebook", "phonebook","phonebook");
 if ($conn === false) {
   die("Ошибка: " . mysqli_connect_error());
 } 
-echo "Подключение успешно установлено";
+$sql = "CREATE TABLE phonebook (id INTEGER AUTO_INCREMENT PRIMARY KEY, name VARCHAR(30), age INTEGER, description VARCHAR(255));";
+if($conn->query($sql)){
+    echo "Таблица phonebook успешно создана";
+} else{
+    echo "Ошибка: " . $conn->error;
+}
 mysqli_close($conn);
 ?>
 </head>
