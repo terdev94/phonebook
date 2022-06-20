c96a677ac97a59646ad5f706608db3a682706af7
Volodimir <terdev94@gmail.com>
2022-06-21 00:00:59 +0300

    Закоментил создание таблицы

--- www/index.php
+++ www/index.php
@@ -7,12 +7,12 @@ $conn = mysqli_connect("database", "phonebook", "phonebook","phonebook");
 if ($conn === false) {
   die("Ошибка: " . mysqli_connect_error());
 } 
-$sql = "CREATE TABLE phonebook (id INTEGER AUTO_INCREMENT PRIMARY KEY, name VARCHAR(30), age INTEGER, description VARCHAR(255));";
+/*$sql = "CREATE TABLE phonebook (id INTEGER AUTO_INCREMENT PRIMARY KEY, name VARCHAR(30), age INTEGER, description VARCHAR(255));";
 if($conn->query($sql)){
-    echo "Таблица phonebook успешно создана";
+   echo "Таблица phonebook успешно создана";
 } else{
     echo "Ошибка: " . $conn->error;
-}
+}*/
 mysqli_close($conn);
 ?>
 </head>
